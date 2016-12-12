2fa56161f120aeb7e90f67c900aaf3cd41d8fe85
Bi Zhiming <bizhiming@vipbcw.com>
2016-12-12 10:05:19 +0800

    集成facebook的xhprof类库

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# xhprof
+# xhprof集成类库
+
+* 安装方法
+
+```
+
+```
+
+* 使用方法
+```
+<?php
+function foo() {
+  for ($idx = 0; $idx < 5; $idx++) {
+    bar($idx);
+    $x = strlen("abc");
+  }
+}
+
+ZyXhprof::startProfiling();
+
+// run program
+foo();
+
+ZyXhprof::endProfiling('demo');
+
+```
---
+++ ZyXhprof.php
@@ -0,0 +1,29 @@
+<?php
+namespace myziyue\xhprof;
+
+/**
+ * Created by PhpStorm.
+ *
+ * @author Bi Zhiming <evan2884@gmail.com>
+ * @created 2016/12/12  上午9:36
+ * @since 1.0
+ */
+defined('MYZY_XHPROF_ROOT') OR define('MYZY_XHPROF_ROOT', realpath(__DIR__));
+
+include_once MYZY_XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+include_once MYZY_XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+
+class ZyXhprof
+{
+    public static function startProfiling()
+    {
+        xhprof_enable();
+    }
+
+    public static function endProfiling($namespace = __FUNCTION__)
+    {
+        $xhprofData = xhprof_disable();
+        $xhprofRuns = new \XHProfRuns_Default();
+        return $xhprofRuns->save_run($xhprofData, $namespace);
+    }
+}
